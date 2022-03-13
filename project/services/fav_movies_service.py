00ceec78cedcc65a2aa109841bbefa7e796a06d7
Marina <petrukmn@yandex.ru>
2022-03-13 20:11:02 +0300

    Add services and views for favorite movies

--- project/services/fav_movies_service.py
+++ project/services/fav_movies_service.py
@@ -1,8 +1,20 @@
 from project.dao.favorite_movie import FavoriteMovieDAO
 from project.exceptions import ItemNotFound
+from project.schemas.movie import MovieSchema
 from project.services.base import BaseService
-from project.config import BaseConfig
 
 
 class FavoriteMoviesService(BaseService):
-    ...
+
+    def get_by_user_id(self, user_id):
+        movies = FavoriteMovieDAO(self._db_session).get_by_user_id(user_id)
+        if not movies:
+            raise ItemNotFound
+        return MovieSchema(many=True).dump(movies)
+
+    def create(self, user_id, mov_id):
+        movie = FavoriteMovieDAO(self._db_session).create(user_id, mov_id)
+        return MovieSchema().dump(movie)
+
+    def delete(self, user_id, mov_id):
+        return FavoriteMovieDAO(self._db_session).delete(user_id, mov_id)
--- project/views/favorite_movies.py
+++ project/views/favorite_movies.py
@@ -4,6 +4,35 @@ from flask_restx import abort, Namespace, Resource
 from project.exceptions import ItemNotFound
 from project.services.fav_movies_service import FavoriteMoviesService
 from project.setup_db import db
-from project.tools.security import auth_required
+from project.tools.security import auth_required, auth_check
 
 fav_movies_ns = Namespace("favorites/movies")
+
+
+@fav_movies_ns.route("/")
+class FavoriteMoviesView(Resource):
+    @auth_required
+    @fav_movies_ns.response(200, "OK")
+    def get(self):
+        """Get all favorite movies"""
+        user_id = auth_check().get('id')
+        try:
+            return FavoriteMoviesService(db.session).get_by_user_id(user_id)
+        except ItemNotFound:
+            abort(404)
+
+
+@fav_movies_ns.route("/<int:mov_id>")
+class FavoriteMovieView(Resource):
+    @auth_required
+    @fav_movies_ns.response(200, "OK")
+    @fav_movies_ns.response(404, "Movie not found")
+    def post(self, mov_id: int):
+        """Add favorite movie"""
+        user_id = auth_check().get('id')
+        return FavoriteMoviesService(db.session).create(user_id, mov_id)
+
+    def delete(self, mov_id: int):
+        """Delete favorite movie"""
+        user_id = auth_check().get('id')
+        return FavoriteMoviesService(db.session).delete(user_id, mov_id)
